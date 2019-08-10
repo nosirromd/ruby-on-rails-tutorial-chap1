6f80e7e8a4f6b6a9cfdc0c934999cb789a208026
dc morrison <nosirromd@gmail.com>
2019-08-10 22:04:50 +0100

    added hello and goodbye routes and html

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: 'hello, world! hola, mundo! ¡hola, mundo!'
+  end
+
+  def goodbye
+    render html: 'goodbye cruel, world! ciao, mundo!' 
+  end
 end
